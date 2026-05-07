7ed804a4383b4cc20c5bc7e5ac9d9f58cbd30b58
Noah Stefano <noahba157@gmail.com>
2026-05-07 04:37:44 +0100

    icon the network and security navigation group

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,7 +34,8 @@ class AdminPanelProvider extends PanelProvider
                 NavigationGroup::make(fn () => trans('admin/dashboard.user'))
                     ->collapsible(false),
                 NavigationGroup::make(fn () => trans('admin/dashboard.server_versions')),
-                NavigationGroup::make('Network & Security'),
+                NavigationGroup::make('Network & Security')
+                    ->icon(TablerIcon::Shield),
                 NavigationGroup::make(fn () => trans('admin/dashboard.advanced')),
             ])
             ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Admin\\Resources')
