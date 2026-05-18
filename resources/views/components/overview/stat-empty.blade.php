1297172c2f83faa34c6e5a7f667d688bc8cb9b3d
Noah Stefano <noahba157@gmail.com>
2026-05-18 02:46:05 +0100

    drop stat-empty component

--- resources/views/components/overview/stat-empty.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['caption' => null])
-
-<p class="overview-stat-card__value overview-stat-card__value--empty">
-    <span class="overview-stat-empty-bar" aria-hidden="true"></span>
-    @if ($caption)
-        <span class="overview-stat-empty-caption">{{ $caption }}</span>
-    @endif
-</p>
