5dd6b604ae271a4078e2e47ce167a36b8e2a6410
Noah Stefano <noahba157@gmail.com>
2026-05-28 19:18:42 +0100

    fact card drops the icon tile for an inline-glyph eyebrow, warn tints the value not the border

--- resources/views/components/overview/fact-cards.blade.php
+++ resources/views/components/overview/fact-cards.blade.php
@@ -3,15 +3,13 @@
 <div class="overview-fact-cards grid grid-cols-3 gap-3">
     @foreach ($cards as $card)
         <div class="overview-fact-card overview-fact-card--{{ $card['variant'] ?? 'default' }}">
-            @if (! empty($card['icon']))
-                <div class="overview-fact-card__icon">
-                    <x-filament::icon :icon="$card['icon']" class="size-3.5" />
-                </div>
-            @endif
-            <div class="overview-fact-card__content">
-                <p class="overview-fact-card__label">{{ $card['label'] }}</p>
-                <p class="overview-fact-card__value">{{ $card['value'] }}</p>
-            </div>
+            <p class="overview-fact-card__label">
+                @if (! empty($card['icon']))
+                    <x-filament::icon :icon="$card['icon']" class="overview-fact-card__icon size-3" />
+                @endif
+                {{ $card['label'] }}
+            </p>
+            <p class="overview-fact-card__value">{{ $card['value'] }}</p>
         </div>
     @endforeach
 </div>
