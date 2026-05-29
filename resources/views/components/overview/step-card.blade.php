ab188313b5e109ff901e8d9a8d838dfbfab572c1
Noah Stefano <noahba157@gmail.com>
2026-05-29 07:03:42 +0100

    rebuild the retrieving overview as one panel with a stepped progress timeline

--- resources/views/components/overview/step-card.blade.php
+++ resources/views/components/overview/step-card.blade.php
@@ -1,15 +1,10 @@
-@props(['title' => null, 'sub' => null, 'steps' => [], 'progress' => false])
+@props(['title' => null, 'sub' => null, 'steps' => []])
 
 <div class="overview-step-card">
     @if ($title || $sub)
         <div class="overview-step-card__header">
             @if ($title)<p class="overview-step-card__title">{{ $title }}</p>@endif
             @if ($sub)<p class="overview-step-card__sub">{{ $sub }}</p>@endif
-        </div>
-    @endif
-    @if ($progress !== false)
-        <div class="overview-step-card__progress">
-            <x-overview.progress-band variant="stashed" :percent="$progress" />
         </div>
     @endif
     <div class="overview-step-card__list">
@@ -19,6 +14,7 @@
                 :active="$step['active'] ?? false"
                 :label="$step['label']"
                 :detail="$step['detail'] ?? null"
+                :percent="$step['percent'] ?? null"
             />
         @endforeach
     </div>
--- resources/views/components/overview/step.blade.php
+++ resources/views/components/overview/step.blade.php
@@ -1,21 +1,28 @@
-@props(['done' => false, 'active' => false, 'label' => '', 'detail' => null])
+@props(['done' => false, 'active' => false, 'label' => '', 'detail' => null, 'percent' => null])
 
 <div @class([
     'overview-step',
     'overview-step--done' => $done,
     'overview-step--active' => $active,
 ])>
-    <span class="overview-step__dot">
-        @if ($done)
-            <x-filament::icon icon="tabler-check" class="size-3" />
-        @elseif ($active)
-            <x-filament::icon icon="tabler-loader-2" class="size-3 overview-step__spinner" />
-        @else
-            <x-filament::icon icon="tabler-point" class="size-3" />
+    <div class="overview-step__row">
+        <span class="overview-step__dot">
+            @if ($done)
+                <x-filament::icon icon="tabler-check" class="size-3" />
+            @elseif ($active)
+                <x-filament::icon icon="tabler-loader-2" class="size-3 overview-step__spinner" />
+            @else
+                <x-filament::icon icon="tabler-point" class="size-3" />
+            @endif
+        </span>
+        <span class="overview-step__label">{{ $label }}</span>
+        @if ($detail)
+            <span class="overview-step__detail">{{ $detail }}</span>
         @endif
-    </span>
-    <span class="overview-step__label">{{ $label }}</span>
-    @if ($detail)
-        <span class="overview-step__detail">{{ $detail }}</span>
+    </div>
+    @if ($percent !== null)
+        <div class="overview-step__progress">
+            <x-overview.progress-band variant="stashed" :percent="$percent" />
+        </div>
     @endif
 </div>
