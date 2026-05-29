c3e15e092c11622e1c553d956a6c019e28c692b9
Noah Stefano <noahba157@gmail.com>
2026-05-29 06:00:57 +0100

    move the retrieve progress strip into the whats happening card

--- resources/views/components/overview/step-card.blade.php
+++ resources/views/components/overview/step-card.blade.php
@@ -1,10 +1,15 @@
-@props(['title' => null, 'sub' => null, 'steps' => []])
+@props(['title' => null, 'sub' => null, 'steps' => [], 'progress' => false])
 
 <div class="overview-step-card">
     @if ($title || $sub)
         <div class="overview-step-card__header">
             @if ($title)<p class="overview-step-card__title">{{ $title }}</p>@endif
             @if ($sub)<p class="overview-step-card__sub">{{ $sub }}</p>@endif
+        </div>
+    @endif
+    @if ($progress !== false)
+        <div class="overview-step-card__progress">
+            <x-overview.progress-band variant="stashed" :percent="$progress" />
         </div>
     @endif
     <div class="overview-step-card__list">
