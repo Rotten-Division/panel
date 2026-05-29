be8124d1c57ad1fe1ef30c98097f472c9f8c2285
Noah Stefano <noahba157@gmail.com>
2026-05-29 20:30:21 +0100

    show retrieve download progress as a ring on the active step node

--- resources/views/components/overview/step.blade.php
+++ resources/views/components/overview/step.blade.php
@@ -1,5 +1,6 @@
 @props(['done' => false, 'active' => false, 'label' => '', 'detail' => null, 'percent' => null])
 
+@php($tracking = $active && ! is_null($percent))
 <div {{ $attributes }} @class([
     'overview-step',
     'overview-step--done' => $done,
@@ -9,6 +10,14 @@
         <span class="overview-step__dot">
             @if ($done)
                 <x-filament::icon icon="tabler-check" class="size-3" />
+            @elseif ($tracking)
+                {{-- pathLength normalises the circle to 100 so the offset is
+                     just 100 - percent. --}}
+                <svg class="overview-step__ring size-3" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+                    <circle class="overview-step__ring-track" cx="12" cy="12" r="9" stroke-width="2" />
+                    <circle class="overview-step__ring-fill" cx="12" cy="12" r="9" stroke-width="2"
+                        pathLength="100" stroke-dasharray="100" style="stroke-dashoffset: {{ 100 - (float) $percent }}" />
+                </svg>
             @elseif ($active)
                 <x-filament::icon icon="tabler-loader-2" class="size-3 overview-step__spinner" />
             @else
@@ -20,9 +29,4 @@
             <span class="overview-step__detail">{{ $detail }}</span>
         @endif
     </div>
-    @if ($percent !== null)
-        <div class="overview-step__progress">
-            <x-overview.progress-band variant="stashed" :percent="$percent" />
-        </div>
-    @endif
 </div>
